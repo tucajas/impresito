def215dc1ca7a5f7a0952c7658501be7c58c7dcc
Jose antonio Suarez <joseantoniosuarez@Mac-mini-de-Jose.local>
2020-10-30 10:37:14 -0300

    claves foraneas

--- impresito_api/models.py
+++ impresito_api/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-"""modelo muchos a muchos proveedores y materia prima"""
+
+# TABLA ORDEN DE TRABAJO
+class OrdenDeTrabajo(models.Model):
+    detalle = models.CharField(max_length=40)
+    descripcionMp = models.CharField(max_length=10)
+    cantMprimaUtilizada = models.IntegerField(default=0)
+    fecha = models.DateField()
+    def __str__(self):
+       return "{}".format(self.detalle)    
+
 
 # TABLA MATERIA PRIMA
 class MateriaPrima( models.Model):
     descripcion = models.CharField(max_length=100)
     stock = models.IntegerField(default=0)
     precio = models.IntegerField(default=0)
+    ordenDeTrabajo = models.ForeignKey(OrdenDeTrabajo, on_delete=models.CASCADE)
     def __str__(self):
         return self.descripcion
 
@@ -27,22 +37,6 @@ class Membership(models.Model):
     materiaprima = models.ForeignKey(MateriaPrima, on_delete=models.CASCADE)
     date_joined = models.DateField()
 
-# TABLA ORDEN DE TRABAJO
-class OrdenDeTrabajo(models.Model):
-    detalle = models.CharField(max_length=40)
-    descripcionMp = models.CharField(max_length=10)
-    cantMprimaUtilizada = models.IntegerField(default=0)
-    fecha = models.DateField()
-    def __str__(self):
-       return "{}".format(self.detalle)    
-
-# TABLA PRODUCTO
-class Producto(models.Model):
-    descripcionP = models.CharField(max_length=40)
-    materiaPriUtilizada = models.CharField(max_length=40)
-    cantiMatPriUtilizada = models.IntegerField(default=0)
-    stock = models.IntegerField(default=0)
-
 # TABLA CLIENTES
 class Cliente( models.Model):
     nombre = models.CharField(max_length=100)
@@ -56,11 +50,19 @@ class Pedido(models.Model):
     descripcion = models.CharField(max_length=40)
     cliente = models.CharField(max_length=10)
     ordenDeTrabajo = models.IntegerField(default=0)
+    idcliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
    
 # TABLA VENTA
 class Venta(models.Model):
     producto = models.CharField(max_length=40)
     precioUnitario = models.IntegerField(default=0)
     precioTotal = models.IntegerField(default=0)
+# TABLA PRODUCTO
+class Producto(models.Model):
+    descripcionP = models.CharField(max_length=40)
+    materiaPriUtilizada = models.CharField(max_length=40)
+    cantiMatPriUtilizada = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
+    idventas = models.ForeignKey(Venta, on_delete=models.CASCADE)
 
     
